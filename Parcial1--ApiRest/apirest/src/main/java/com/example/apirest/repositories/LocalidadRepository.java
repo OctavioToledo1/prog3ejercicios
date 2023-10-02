4fc1afd6c4bd02bd6c1f810bf3fc57b9ff1d9037
OctavioToledo1 <130512636+OctavioToledo1@users.noreply.github.com>
2023-10-02 03:19:28 -0300

    Add files via upload

---
+++ Parcial1--ApiRest/apirest/src/main/java/com/example/apirest/repositories/LocalidadRepository.java
@@ -0,0 +1,7 @@
+package com.example.apirest.repositories;
+
+import com.example.apirest.entities.Localidad;
+import com.fasterxml.jackson.databind.ser.Serializers;
+
+public interface LocalidadRepository extends BaseRepository<Localidad, Long> {
+}
